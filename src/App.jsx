b0a66df45dfeef6b6e1f18ff714b4ec27a5aa916
Gabriel Beltzer <gabriel.beltzer0@gmail.com>
2025-12-04 22:29:57 +0100

    Ajout de route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Contact from "./contact";
 import GifLogin from "./GifLogin";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-  return <GifLogin />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GifLogin />}/> 
+        <Route path="/contact" element={<Contact />}/> 
+      </Routes>
+    </BrowserRouter>
+  )
 }
---
+++ src/contact.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import { Search, User, LogOut, Image as ImageIcon, Sparkles, ArrowRight, X } from "lucide-react";
+import "./index.css"; // Ensure styles are applied
+
+export default function Contact() {
+  return (
+    <p>hello</p>
+  )
+}
