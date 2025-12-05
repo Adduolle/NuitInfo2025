f7e7322c4661955c283fb35c79463ae9d4b4644f
Adduolle <Adrien.duolle@gmail.com>
2025-12-05 04:21:14 +0100

    passage de profiletest to profile

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Contact from "./contact";
 import GifLogin from "./GifLogin";
 import ClickTrapQuiz from "./ClickTrapQuiz";
 import Spawn from "./Spawn"; 
-import ProfileTest from "./ProfileTest";
+import Profile from "./Profile";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
@@ -13,7 +13,7 @@ export default function App() {
         <Route path="/login" element={<GifLogin />}/> 
         <Route path="/quiz-click-trap" element={<ClickTrapQuiz />}/> 
         <Route path="/contact" element={<Contact />}/> 
-        <Route path="/ProfileTest" element={<ProfileTest />}/> 
+        <Route path="/Profile" element={<Profile />}/> 
       </Routes>
     </BrowserRouter>
   )
